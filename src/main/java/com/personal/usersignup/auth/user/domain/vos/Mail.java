bc3cc69bc5e31b393d5386ec9d9fa0ffbd607c7f
Fernando A. Perera <fernando02perera@gmail.com>
2024-09-03 23:23:47 +0200

    :test_tube: should_not_create_mail_if_content_is_empty, not implemented yet

---
+++ src/main/java/com/personal/usersignup/auth/user/domain/vos/Mail.java
@@ -0,0 +1,19 @@
+package com.personal.usersignup.auth.user.domain.vos;
+
+import com.personal.usersignup.auth.user.domain.errors.MailCannotBeEmpty;
+import com.personal.usersignup.shared.Result;
+import com.personal.usersignup.shared.domain.definition.ValueObject;
+import com.personal.usersignup.shared.domain.error.DomainError;
+
+public class Mail extends ValueObject {
+
+    private String value;
+
+    private Mail(String value) {
+        this.value = value;
+    }
+
+    public static Result<DomainError, Mail> of(String mail) {
+        throw new RuntimeException("Not implemented yet");
+    }
+}
---
+++ src/test/java/com/personal/usersignup/auth/user/domain/vos/MailTest.java
@@ -0,0 +1,27 @@
+package com.personal.usersignup.auth.user.domain.vos;
+
+import com.personal.usersignup.auth.user.domain.errors.MailCannotBeEmpty;
+import com.personal.usersignup.shared.Result;
+import com.personal.usersignup.shared.domain.error.DomainError;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailTest {
+
+    // empty string -> domain error
+
+    @Test
+    void should_not_create_mail_if_content_is_empty() {
+        String content = "";
+
+        Result<DomainError, Mail> result = Mail.of(content).map(
+                error -> assertInstanceOf(MailCannotBeEmpty.class, error),
+                mail -> {
+                    assertNull(mail);
+                    return null;
+                }
+        );
+    }
+
+}
