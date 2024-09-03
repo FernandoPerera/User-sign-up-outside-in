0a4ccd1210364b5c5ff36d5ea43d8ce2877afd0d
Fernando A. Perera <fernando02perera@gmail.com>
2024-09-03 23:15:52 +0200

    :sparkles: created class to wrap method results

---
+++ src/main/java/com/personal/usersignup/shared/Result.java
@@ -0,0 +1,40 @@
+package com.personal.usersignup.shared;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Result<E, T> {
+
+    private T result;
+    private E error;
+    private  boolean isError = false;
+
+    public <U> Result<E, U> map(Function<? super E, ? extends E> errorMapper, Function<? super T, ? extends U> successMapper) {
+        if (isError) {
+            return Result.error(errorMapper.apply(error));
+        } else {
+            return Result.success(successMapper.apply(result));
+        }
+    }
+
+    public static <E, T> Result<E, T> error(E error) {
+        Result<E, T> result = new Result<>();
+        result.isError = true;
+        result.error = error;
+        return result;
+    }
+
+    public static <E, T> Result<E, T> success(T value) {
+        Result<E, T> result = new Result<>();
+        result.result = value;
+        return result;
+    }
+
+    public Optional<E> getError() {
+        return isError ? Optional.of(error) : Optional.empty();
+    }
+
+    public Optional<T> getResult() {
+        return isError ? Optional.empty() : Optional.of(result);
+    }
+}
