3fa296ee2ca8b17ce9fb5027d2b4094b05da6ff1
fernandoperera <fernando02perera@gmail.com>
2024-10-08 19:15:31 +0100

    :recycle: added value object for mail user attribute

--- src/main/java/com/personal/usersignup/auth/user/domain/models/User.java
+++ src/main/java/com/personal/usersignup/auth/user/domain/models/User.java
@@ -1,19 +1,20 @@
 package com.personal.usersignup.auth.user.domain.models;
 
 import com.personal.usersignup.auth.user.domain.records.read.UserDefinition;
+import com.personal.usersignup.auth.user.domain.vos.Mail;
 
 public class User {
-    private String mail;
+    private Mail mail;
     private String username;
     private String password;
 
-    private User(String mail, String username, String password) {
+    private User(Mail mail, String username, String password) {
         this.mail = mail;
         this.username = username;
         this.password = password;
     }
 
-    public static User create(String mail, String username, String password) {
+    public static User create(Mail mail, String username, String password) {
         return new User(mail, username, password);
     }
 
