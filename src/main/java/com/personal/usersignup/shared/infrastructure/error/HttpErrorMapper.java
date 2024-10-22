2472dd151d3f7b3698a42b48495257c0583078a4
fernandoperera <fernando02perera@gmail.com>
2024-10-22 23:37:27 +0100

    :sparkles: transformed HttpErrorMapper class to abstract class

--- src/main/java/com/personal/usersignup/shared/infrastructure/error/HttpErrorMapper.java
+++ src/main/java/com/personal/usersignup/shared/infrastructure/error/HttpErrorMapper.java
@@ -1,18 +1,17 @@
 package com.personal.usersignup.shared.infrastructure.error;
 
-import com.personal.usersignup.auth.user.domain.errors.MailCannotBeEmpty;
-import com.personal.usersignup.auth.user.domain.errors.MailHaveWrongFormat;
 import com.personal.usersignup.shared.domain.error.DomainError;
 import org.springframework.http.HttpStatus;
 
-public class HttpErrorMapper {
+public abstract class HttpErrorMapper {
 
-    public static HttpStatus resolve(DomainError error) {
+    protected abstract HttpStatus resolveSpecificError(DomainError error);
 
-        return switch (error) {
-            case MailCannotBeEmpty mail -> HttpStatus.BAD_REQUEST;
-            case MailHaveWrongFormat mail -> HttpStatus.BAD_REQUEST;
-            default -> HttpStatus.INTERNAL_SERVER_ERROR;
-        };
+    public HttpStatus resolve(DomainError error) {
+        boolean isUnknownError = error == null;
+        if (isUnknownError) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return resolveSpecificError(error);
     }
 }
