5a0a1e15c3ab973ba86f7872d77d8c5af651f4db
fernandoperera <fernando02perera@gmail.com>
2024-10-28 19:39:41 +0000

    :recycle: replaced primitive to Username value object

--- src/main/java/com/personal/usersignup/auth/user/domain/models/User.java
+++ src/main/java/com/personal/usersignup/auth/user/domain/models/User.java
@@ -2,23 +2,24 @@ package com.personal.usersignup.auth.user.domain.models;
 
 import com.personal.usersignup.auth.user.domain.records.read.UserDefinition;
 import com.personal.usersignup.auth.user.domain.vos.Mail;
+import com.personal.usersignup.auth.user.domain.vos.Username;
 
 public class User {
     private Mail mail;
-    private String username;
+    private Username username;
     private String password;
 
-    private User(Mail mail, String username, String password) {
+    private User(Mail mail, Username username, String password) {
         this.mail = mail;
         this.username = username;
         this.password = password;
     }
 
-    public static User create(Mail mail, String username, String password) {
+    public static User create(Mail mail, Username username, String password) {
         return new User(mail, username, password);
     }
 
     public UserDefinition toDefinition(String token) {
-        return new UserDefinition(this.username, token);
+        return new UserDefinition(this.username.getValue(), token);
     }
 }
